5ad1f49ff9c73c0526c597ea59e7c1b72bfd8f1d
Ayushrai2005 <shweetarai@gmail.com>
2024-05-02 02:00:38 +0530

    Created Remaining Routes for Posts -> Get Post by id  , delete post by id , get Feed posts and posts from user ID

--- src/main/kotlin/instaU/ayush/com/route/PostRoute.kt
+++ src/main/kotlin/instaU/ayush/com/route/PostRoute.kt
@@ -4,11 +4,13 @@ import instaU.ayush.com.model.PostResponse
 import instaU.ayush.com.model.PostTextParams
 import instaU.ayush.com.repository.post.PostRepository
 import instaU.ayush.com.util.Constants
+import instaU.ayush.com.util.getLongParameter
 import instaU.ayush.com.util.saveFile
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.plugins.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -60,6 +62,112 @@ fun Routing.postRouting(){
                 }
             }
 
+            get(path = "/{postId}"){
+                try {
+                    val postId = call.getLongParameter("postId")
+                    val currentUserId = call.getLongParameter("currentUserId", true)
+
+                    val result = postRepository.getPost(postId, currentUserId)
+                    call.respond(
+                        status = result.code,
+                        message = result.data
+                    )
+                }
+                catch (e: BadRequestException){
+                    return@get
+                }
+                catch (e: Throwable){
+                    call.respond(
+                        status = HttpStatusCode.InternalServerError,
+                        message = PostResponse(
+                            success = false,
+                            message = "An unexpected error has occurred, try again!"
+                        )
+                    )
+                }
+            }
+
+            delete(path = "/{postId}"){
+                try {
+                    val postId = call.getLongParameter("postId")
+                    val result = postRepository.deletePost(postId)
+                    call.respond(
+                        status = result.code,
+                        message = result.data
+                    )
+                }
+                catch (e: BadRequestException){
+                    return@delete
+                }
+                catch (e: Throwable){
+                    call.respond(
+                        status = HttpStatusCode.InternalServerError,
+                        message = PostResponse(
+                            success = false,
+                            message = "An unexpected error has occurred, try again!"
+                        )
+                    )
+                }
+            }
+
+        }
+
+        route(path = "/posts"){
+            get(path = "/feed"){
+                try {
+                    val currentUserId = call.getLongParameter("currentUserId", true)
+                    val page = call.request.queryParameters["page"]?.toIntOrNull() ?: 0
+                    val limit = call.request.queryParameters["limit"]?.toIntOrNull() ?: Constants.DEFAULT_PAGE_SIZE
+                    val result = postRepository.getFeedsPost(currentUserId, page, limit)
+                    call.respond(
+                        status = result.code,
+                        message = result.data
+                    )
+                }
+                catch (e: BadRequestException){
+                    return@get
+                }
+                catch (e: Throwable){
+                    call.respond(
+                        status = HttpStatusCode.InternalServerError,
+                        message = PostResponse(
+                            success = false,
+                            message = "An unexpected error has occurred, try again!"
+                        )
+                    )
+                }
+            }
+
+            get(path = "/{userId}"){
+                try {
+                    val postOwnerId = call.getLongParameter("userId")
+                    val currentUserId = call.getLongParameter("currentUserId", true)
+                    val page = call.request.queryParameters["page"]?.toIntOrNull() ?: 0
+                    val limit = call.request.queryParameters["limit"]?.toIntOrNull() ?: Constants.DEFAULT_PAGE_SIZE
+                    val result = postRepository.getPostByUser(
+                        postOwnerId = postOwnerId,
+                        currentUserId = currentUserId,
+                        pageNumber = page,
+                        pageSize = limit
+                    )
+                    call.respond(
+                        status = result.code,
+                        message = result.data
+                    )
+                }
+                catch (e: BadRequestException){
+                    return@get
+                }
+                catch (e: Throwable){
+                    call.respond(
+                        status = HttpStatusCode.InternalServerError,
+                        message = PostResponse(
+                            success = false,
+                            message = "An unexpected error has occurred, try again!"
+                        )
+                    )
+                }
+            }
         }
 
     }
